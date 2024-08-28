557dc1f2ab7d9f84d6b68c1046d8c504cfe91751
Sarath Kumar. S <107998949+Sarathsanthanam@users.noreply.github.com>
2024-08-28 19:00:25 +0530

    Update Task 2.sql

--- Task 2.sql
+++ Task 2.sql
@@ -1,26 +1,19 @@
-CREATE TABLE People (
-    PersonID INT IDENTITY(1,1) PRIMARY KEY,
-    Gender VARCHAR(10),
-    Age INT
+
+CREATE TABLE IF NOT EXISTS people (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gender VARCHAR(10),
+    age INT
 );
 
 
-
-INSERT INTO People (Gender, Age)
-SELECT 'Girl', CASE 
-    WHEN RAND() < 0.18 THEN FLOOR(RAND() * 6) + 7  
-    WHEN RAND() < 0.45 THEN FLOOR(RAND() * 2) + 13 
-    WHEN RAND() < 0.65 THEN FLOOR(RAND() * 3) + 15 
-    ELSE FLOOR(RAND() * 2) + 18                  
-END
-FROM (SELECT TOP 3250 * FROM table_name) AS tmp;
-
-
-INSERT INTO People (Gender, Age)
-SELECT 'Boy', CASE 
-    WHEN RAND() < 0.18 THEN FLOOR(RAND() * 6) + 7  
-    WHEN RAND() < 0.45 THEN FLOOR(RAND() * 2) + 13 
-    WHEN RAND() < 0.65 THEN FLOOR(RAND() * 3) + 15 
-    ELSE FLOOR(RAND() * 2) + 18                   
-END
-FROM (SELECT TOP 1750 * FROM table_name) AS tmp;
+INSERT INTO people (gender, age)
+SELECT 
+    IF(RAND() <= 0.65, 'Girl', 'Boy') AS gender,
+    CASE 
+        WHEN RAND() <= 0.18 THEN FLOOR(7 + RAND() * 6)  
+        WHEN RAND() <= 0.45 THEN FLOOR(13 + RAND() * 2) 
+        WHEN RAND() <= 0.65 THEN FLOOR(15 + RAND() * 3) 
+        ELSE FLOOR(18 + RAND() * 2)                     
+    END AS age
+FROM 
+    (SELECT 1 FROM table_name LIMIT 5000) t;
